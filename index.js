944080d7f549b36735d253e0efc9d76393a6d7be
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-12-05 16:45:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const metricsql = require('./src');
 process.argv.splice(0, 2);
 var query =  process.argv[0] || `sum(rate(foo{bar="baz"}[5m])) by (x,y)`
 try {
-  const parsed = JSON.parse(metricsql.parse(query));
+  const parsed = metricsql.parse(query);
   console.log(parsed);
-  return parsed;
 } catch(e){
   console.log(e); 
 }
